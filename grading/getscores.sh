a18076efc677f40d943d683407d06533741bda92
Eric Seidel <gridaphobe@gmail.com>
2015-02-02 11:02:02 -0800

    wi15/hw1 scores

--- grading/getscores.sh
+++ grading/getscores.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-HW=$4
+HW=$1
 
-for dir in `find "../studentfiles/" -name "HW$HW-*-graded"`; do
+for dir in `find "studentfiles/" -name "HW$HW-*-graded"`; do
     studentdir=`dirname $dir`
     student=`basename $studentdir`
-    score=`tail -3 $dir/gradereport | head -1 | cut -d " " -f 11`
+    score=`tail -1 $dir/gradereport | head -1 | cut -d " " -f 11`
+    if test -z "$score"; then
+      score="0"
+    fi
     echo "$student, $score"
 done
